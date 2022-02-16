35f9811bab0c761ff3643108008028c18f51f9d2
Liam <liammiles1234@hotmail.com>
2022-02-16 18:44:01 +0000

    Added score percentage Changed how per hand average cut is displayed Moved the current score Added auto re-connecting to HTTP status' web socket Changed how data resetting works

--- components/ScoreStats.js
+++ components/ScoreStats.js
@@ -6,9 +6,11 @@ export default class ScoreStats extends Component {
         super(params);
 
         this.state = {
-            score: 0,
-            dataSocket: undefined,
-            isVisible: false,
+            socket: undefined,
+            isVisible: true,
+            currentScore: 0,
+            percentage: "100.00%",
+            failed: false,
             leftHand: {
                 averageCut: [15.00],
                 averagePreSwing: [70.00],
@@ -22,130 +24,140 @@ export default class ScoreStats extends Component {
         }
     }
 
-    componentDidMount() {
+    connectSocket() {
         const socket = new WebSocket('ws://localhost:6557/socket');
-        this.setState({ socket: socket });
         socket.addEventListener('error' || 'close', () => {
-            console.log("Attempting to re-connect to the HTTP Status socket.");
-            this.setState({ socket: new WebSocket('ws://localhost:6557/socket') });
+            console.log("Attempting to re-connect to the HTTP Status socket in 30 seconds.");
+            setTimeout(() => this.connectSocket(), 30_000);
         });
         socket.addEventListener('message', (message) => {
-            //console.log("Received message from HTTP Status.");
             const json = JSON.parse(message.data);
-            if (!handlers[json.event]) {
+            if (!this.handlers[json.event]) {
                 console.log("Unhandled message from HTTP Status. (" + json.event + ")");
                 return;
             }
-            handlers[json.event](json || []);
+            this.handlers[json.event](json || []);
         })
+        this.setState({ socket: socket });
+    }
 
-        const handlers = {
-            "hello": () => {
-                console.log("Hello from HTTP Status!");
+    resetData(visible) {
+        console.log("Exiting level, resetting data.")
+        this.setState({
+            "leftHand": {
+                "averageCut": [15.00],
+                "averagePreSwing": [70.00],
+                "averagePostSwing": [30.00],
             },
-            "scoreChanged": (data) => {
-                const { status } = data;
-                this.setState({ score: status.performance.score })
+            "rightHand": {
+                "averageCut": [15.00],
+                "averagePreSwing": [70.00],
+                "averagePostSwing": [30.00],
             },
-            "noteFullyCut": (data) => {
-                const { noteCut } = data;
+            currentScore: 0,
+            percentage: "100.00%",
+            isVisible: visible
+        });
+    }
 
-                // Left Saber
-                if (noteCut.saberType === 'SaberA') {
-                    const data = this.state.leftHand;
-                    if (data.averageCut.includes(15) && data.averageCut.length === 1) {
-                        data.averageCut = [];
-                    }
-                    if (data.averagePreSwing.includes(70) && data.averagePreSwing.length === 1) {
-                        data.averagePreSwing = [];
-                    }
-                    if (data.averagePostSwing.includes(30) && data.averagePostSwing.length === 1) {
-                        data.averagePostSwing = [];
-                    }
-                    data.averagePreSwing.push(noteCut.initialScore > 70 ? 70 : noteCut.initialScore);
-                    data.averagePostSwing.push(noteCut.finalScore - noteCut.initialScore);
-                    data.averageCut.push(noteCut.cutDistanceScore);
-                    this.setState({ leftHand: data });
+    handlers = {
+        "hello": () => {
+            console.log("Hello from HTTP Status!");
+        },
+        "scoreChanged": (data) => {
+            const { status } = data;
+            const { score, currentMaxScore } = status.performance;
+            const percent = currentMaxScore > 0 ? ((score / currentMaxScore) * 1000 / 10).toFixed(2) : 0.00;
+            this.setState({
+                    currentScore: score,
+                    percentage: this.state.failed ? percent * 2 : percent + "%"
+            })
+        },
+        "noteFullyCut": (data) => {
+            const { noteCut } = data;
+
+            // Left Saber
+            if (noteCut.saberType === 'SaberA') {
+                const data = this.state.leftHand;
+                if (data.averageCut.includes(15) && data.averageCut.length === 1) {
+                    data.averageCut = [];
                 }
-
-                // Left Saber
-                if (noteCut.saberType === 'SaberB') {
-                    const data = this.state.rightHand;
-                    if (data.averageCut.includes(15) && data.averageCut.length === 1) {
-                        data.averageCut = [];
-                    }
-                    if (data.averagePreSwing.includes(70) && data.averagePreSwing.length === 1) {
-                        data.averagePreSwing = [];
-                    }
-                    if (data.averagePostSwing.includes(30) && data.averagePostSwing.length === 1) {
-                        data.averagePostSwing = [];
-                    }
-                    data.averagePreSwing.push(noteCut.initialScore > 70 ? 70 : noteCut.initialScore);
-                    data.averagePostSwing.push(noteCut.finalScore - noteCut.initialScore);
-                    data.averageCut.push(noteCut.cutDistanceScore);
-                    this.setState({ rightHand: data });
+                if (data.averagePreSwing.includes(70) && data.averagePreSwing.length === 1) {
+                    data.averagePreSwing = [];
                 }
-            },
-            "menu": () => {
-                console.log("Exiting level, resetting data.")
-                this.setState({
-                    "leftHand": {
-                        "averageCut": [15.00],
-                        "averagePreSwing": [70.00],
-                        "averagePostSwing": [30.00],
-                    },
-                    "rightHand": {
-                        "averageCut": [15.00],
-                        "averagePreSwing": [70.00],
-                        "averagePostSwing": [30.00],
-                    },
-                    score: 0,
-                    isVisible: false
-                });
-            },
-            "songStart": () => {
-                console.log("Going into level, resetting data.")
-                this.setState({
-                    "leftHand": {
-                        "averageCut": [15.00],
-                        "averagePreSwing": [70.00],
-                        "averagePostSwing": [30.00],
-                    },
-                    "rightHand": {
-                        "averageCut": [15.00],
-                        "averagePreSwing": [70.00],
-                        "averagePostSwing": [30.00],
-                    },
-                    score: 0,
-                    isVisible: true
-                });
-            },
-            "noteCut": () => {},
-            "noteMissed": () => {},
-            "noteSpawned": () => {},
-            "bombMissed": () => {},
-            "beatmapEvent": () => {}
-        }
+                if (data.averagePostSwing.includes(30) && data.averagePostSwing.length === 1) {
+                    data.averagePostSwing = [];
+                }
+                data.averagePreSwing.push(noteCut.initialScore > 70 ? 70 : noteCut.initialScore);
+                data.averagePostSwing.push(noteCut.finalScore - noteCut.initialScore);
+                data.averageCut.push(noteCut.cutDistanceScore);
+                this.setState({ leftHand: data });
+            }
+
+            // Left Saber
+            if (noteCut.saberType === 'SaberB') {
+                const data = this.state.rightHand;
+                if (data.averageCut.includes(15) && data.averageCut.length === 1) {
+                    data.averageCut = [];
+                }
+                if (data.averagePreSwing.includes(70) && data.averagePreSwing.length === 1) {
+                    data.averagePreSwing = [];
+                }
+                if (data.averagePostSwing.includes(30) && data.averagePostSwing.length === 1) {
+                    data.averagePostSwing = [];
+                }
+                data.averagePreSwing.push(noteCut.initialScore > 70 ? 70 : noteCut.initialScore);
+                data.averagePostSwing.push(noteCut.finalScore - noteCut.initialScore);
+                data.averageCut.push(noteCut.cutDistanceScore);
+                this.setState({ rightHand: data });
+            }
+        },
+        "songStart": () => {
+            console.log("Going into level, resetting data.")
+            this.resetData(true);
+        },
+        "finished": () => {
+            console.log("Exiting level, resetting data.")
+            this.resetData(false);
+        },
+        "softFail": () => {
+            this.setState({ failed: true });
+        },
+        "menu": () => {},
+        "noteCut": () => {},
+        "noteMissed": () => {},
+        "noteSpawned": () => {},
+        "bombMissed": () => {},
+        "beatmapEvent": () => {}
+    }
+
+    componentDidMount() {
+        this.connectSocket();
     }
 
     getAverage(values) {
-        return values.reduce( ( p, c ) => p + c, 0 ) / values.length;
+        return values.reduce((p, c) => p + c, 0) / values.length;
     }
 
     render() {
         return <div className={'score-stats'} style={{ display: this.state.isVisible ? "block" : "none" }}>
-            <p>{this.state.score.toLocaleString()}</p>
+            <p className={'score-stats-average-cut'}>Average Cut</p>
             <div className={'score-stats-hands'}>
-                <div>
+                <div className={'score-stats-left'}>
                     <p>{this.getAverage(this.state.leftHand.averagePreSwing).toFixed(2)}</p>
                     <p>{this.getAverage(this.state.leftHand.averagePostSwing).toFixed(2)}</p>
                     <p>{this.getAverage(this.state.leftHand.averageCut).toFixed(2)}</p>
                 </div>
-                <div>
+                <div className={'score-stats-right'}>
                     <p>{this.getAverage(this.state.rightHand.averagePreSwing).toFixed(2)}</p>
                     <p>{this.getAverage(this.state.rightHand.averagePostSwing).toFixed(2)}</p>
                     <p>{this.getAverage(this.state.rightHand.averageCut).toFixed(2)}</p>
                 </div>
+            </div>
+
+            <div className={'score-stats-info'}>
+                <p>{this.state.percentage}</p>
+                <p>{this.state.currentScore.toLocaleString()}</p>
             </div>
         </div>
     }
---
+++ components/SongInfo.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+export default class SongInfo extends Component {
+
+    constructor(params) {
+        super(params);
+    }
+
+    render() {
+        return <div className={'song-info'}>
+
+        </div>
+    }
+}

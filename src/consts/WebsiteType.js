722c4ddec780e7d625f316cc416122537f3e7be4
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-10-19 17:15:45 +0100

    Changed to the basic SS endpoint

--- src/consts/WebsiteType.js
+++ src/consts/WebsiteType.js
@@ -2,7 +2,7 @@ import Config from "../../config.json";
 
 const WebsiteTypes = {
 	ScoreSaber: {
-		ApiUrl: Config.proxy_url + "/https://scoresaber.com/api/player/%s/full",
+		ApiUrl: Config.proxy_url + "/https://scoresaber.com/api/player/%s/basic",
 	},
 	BeatLeader: {
 		ApiUrl: Config.proxy_url + "/https://api.beatleader.xyz/player/%s",

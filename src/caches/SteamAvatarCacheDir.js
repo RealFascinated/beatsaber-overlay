0aa5be8bc9b529e30ee38a0ac151075bbcd18bd9
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-10-19 18:11:59 +0100

    Update avatar cache location

--- src/caches/SteamAvatarCacheDir.js
+++ src/caches/SteamAvatarCacheDir.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const cacheDir = process.cwd() + path.sep + "cache";
+const cacheDir = process.cwd() + path.sep + "avatar_cache";
 if (!fs.existsSync(cacheDir)) {
 	fs.mkdirSync(cacheDir);
 	console.log("Created avatar cache directory");

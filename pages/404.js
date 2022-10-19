54902eb7b79ea9324d06407bfa7418803269a2c6
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-10-19 20:13:26 +0100

    Add basic 404 page

---
+++ pages/404.js
@@ -0,0 +1,51 @@
+import { Container, Grid, Image, Text } from "@nextui-org/react";
+import { Component } from "react";
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<Container css={{
+        marginTop: '$15'
+      }}>
+				<Grid.Container gap={2}>
+					<Grid
+						xs={12}
+						sm={6}
+						justify="center"
+						css={{
+							textAlign: "center",
+						}}
+					>
+						<div
+							style={{
+								marginTop: "30px",
+							}}
+						>
+							<Text h2 b>Whoops! This page is unknown!</Text>
+							<Text h3>
+								To visit the main website click{" "}
+								<span>
+									<a href="/">here</a>
+								</span>
+							</Text>
+						</div>
+					</Grid>
+					<Grid xs={12} sm={6} justify="center">
+						<Image
+							alt="Helpful doggo"
+							src="https://cdn.fascinated.cc/fPKqSysuqz.jpg?raw=true"
+							css={{
+								borderRadius: "5%",
+								display: "initial",
+							}}
+						></Image>
+					</Grid>
+				</Grid.Container>
+			</Container>
+		);
+	}
+}

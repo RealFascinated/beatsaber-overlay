095a4d289abfdc58dd51557fe5e4583ba4dd24a2
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-10-26 15:07:33 +0100

    stuffs

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,8 @@ test -n "$NEXT_PUBLIC_SITE_COLOR"
 echo "Check that we have NEXT_PUBLIC_SITE_URL vars"
 test -n "$NEXT_PUBLIC_SITE_URL"
 
+echo find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 
+
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_HTTP_PROXY#$NEXT_PUBLIC_HTTP_PROXY#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_NAME#$NEXT_PUBLIC_SITE_NAME#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_TITLE#$NEXT_PUBLIC_SITE_TITLE#g"

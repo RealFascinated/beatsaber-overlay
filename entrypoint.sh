4e0376eede5aa6d5398a85915b0135fc4d621925
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-10-26 16:25:19 +0100

    once again new delim

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-\\!/bin/sh
+$'\001'!/bin/sh
 
 echo "Checking that NEXT_PUBLIC_API_URL env-var exists"
 test -n "$NEXT_PUBLIC_HTTP_PROXY"
@@ -26,12 +26,12 @@ echo "Found $NEXT_PUBLIC_SITE_URL"
 
 echo "Updating static next env vars"
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s\\APP_NEXT_PUBLIC_HTTP_PROXY\\$NEXT_PUBLIC_HTTP_PROXY\\g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s\\APP_NEXT_PUBLIC_SITE_NAME\\$NEXT_PUBLIC_SITE_NAME\\g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s\\APP_NEXT_PUBLIC_SITE_TITLE\\$NEXT_PUBLIC_SITE_TITLE\\g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s\\APP_NEXT_PUBLIC_SITE_DESCRIPTION\\$NEXT_PUBLIC_SITE_DESCRIPTION\\g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s\\APP_NEXT_PUBLIC_SITE_COLOR\\$NEXT_PUBLIC_SITE_COLOR\\g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s\\APP_NEXT_PUBLIC_SITE_URL\\$NEXT_PUBLIC_SITE_URL\\g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s$'\001'APP_NEXT_PUBLIC_HTTP_PROXY$'\001'$NEXT_PUBLIC_HTTP_PROXY$'\001'g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s$'\001'APP_NEXT_PUBLIC_SITE_NAME$'\001'$NEXT_PUBLIC_SITE_NAME$'\001'g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s$'\001'APP_NEXT_PUBLIC_SITE_TITLE$'\001'$NEXT_PUBLIC_SITE_TITLE$'\001'g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s$'\001'APP_NEXT_PUBLIC_SITE_DESCRIPTION$'\001'$NEXT_PUBLIC_SITE_DESCRIPTION$'\001'g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s$'\001'APP_NEXT_PUBLIC_SITE_COLOR$'\001'$NEXT_PUBLIC_SITE_COLOR$'\001'g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s$'\001'APP_NEXT_PUBLIC_SITE_URL$'\001'$NEXT_PUBLIC_SITE_URL$'\001'g"
 
 echo "Starting NextJS"
 exec "$@"

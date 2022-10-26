e035727881912dcaeb55b94fa12bf3d1fce78aa2
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-10-26 16:19:20 +0100

    maybe fix ?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+,!/bin/sh
 
 echo "Checking that NEXT_PUBLIC_API_URL env-var exists"
 test -n "$NEXT_PUBLIC_HTTP_PROXY"
@@ -26,12 +26,12 @@ echo "Found $NEXT_PUBLIC_SITE_URL"
 
 echo "Ignore permission errors below"
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_HTTP_PROXY#$NEXT_PUBLIC_HTTP_PROXY#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_NAME#$NEXT_PUBLIC_SITE_NAME#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_TITLE#$NEXT_PUBLIC_SITE_TITLE#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_DESCRIPTION#$NEXT_PUBLIC_SITE_DESCRIPTION#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_COLOR#$NEXT_PUBLIC_SITE_COLOR#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_SITE_URL#$NEXT_PUBLIC_SITE_URL#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s,APP_NEXT_PUBLIC_HTTP_PROXY,$NEXT_PUBLIC_HTTP_PROXY,g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s,APP_NEXT_PUBLIC_SITE_NAME,$NEXT_PUBLIC_SITE_NAME,g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s,APP_NEXT_PUBLIC_SITE_TITLE,$NEXT_PUBLIC_SITE_TITLE,g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s,APP_NEXT_PUBLIC_SITE_DESCRIPTION,$NEXT_PUBLIC_SITE_DESCRIPTION,g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s,APP_NEXT_PUBLIC_SITE_COLOR,$NEXT_PUBLIC_SITE_COLOR,g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s,APP_NEXT_PUBLIC_SITE_URL,$NEXT_PUBLIC_SITE_URL,g"
 
 echo "Starting NextJS"
 exec "$@"

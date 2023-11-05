6b5b8bb54f494d2e9e0d537747d732c940a22ebc
Liam <liam@fascinated.cc>
2023-11-05 23:17:44 +0000

    add deprecation notice

--- src/pages/overlay.js
+++ src/pages/overlay.js
@@ -12,71 +12,86 @@ import { usePlayerDataStore } from "../store/playerDataStore";
 import styles from "../styles/overlay.module.css";
 
 export default function Overlay(props) {
-	const query = JSON.parse(props.query);
-	const [setOverlaySettings, mounted, setMounted] = useSettingsStore(
-		(state) => [state.setOverlaySettings, state.mounted, state.setMounted]
-	);
-	const updatePlayerData = usePlayerDataStore(
-		(state) => state.updatePlayerData
-	);
+  const query = JSON.parse(props.query);
+  const [setOverlaySettings, mounted, setMounted] = useSettingsStore(
+    (state) => [state.setOverlaySettings, state.mounted, state.setMounted]
+  );
+  const updatePlayerData = usePlayerDataStore(
+    (state) => state.updatePlayerData
+  );
 
-	useEffect(() => {
-		if (!mounted && props.isValidSteamId) {
-			setMounted(true);
+  useEffect(() => {
+    if (!mounted && props.isValidSteamId) {
+      setMounted(true);
 
-			async function setup() {
-				await setOverlaySettings(query);
-				const showSongInfo = useSettingsStore.getState().showSongInfo;
-				const showScoreInfo = useSettingsStore.getState().showScoreInfo;
-				if (showSongInfo || (showScoreInfo && typeof window !== "undefined")) {
-					await connectClient();
-				}
-				const showPlayerStats = useSettingsStore.getState().showPlayerStats;
-				if (showPlayerStats) {
-					await updatePlayerData();
-				}
-			}
-			setup();
-		}
-	}, [
-		query,
-		props.isValidSteamId,
-		setOverlaySettings,
-		mounted,
-		setMounted,
-		updatePlayerData,
-	]);
+      async function setup() {
+        await setOverlaySettings(query);
+        const showSongInfo = useSettingsStore.getState().showSongInfo;
+        const showScoreInfo = useSettingsStore.getState().showScoreInfo;
+        if (showSongInfo || (showScoreInfo && typeof window !== "undefined")) {
+          await connectClient();
+        }
+        const showPlayerStats = useSettingsStore.getState().showPlayerStats;
+        if (showPlayerStats) {
+          await updatePlayerData();
+        }
+      }
+      setup();
+    }
+  }, [
+    query,
+    props.isValidSteamId,
+    setOverlaySettings,
+    mounted,
+    setMounted,
+    updatePlayerData,
+  ]);
 
-	if (!props.isValidSteamId) {
-		return (
-			<div className={styles.invalidPlayer}>
-				<h1>Invalid Steam ID</h1>
-				<h3>Please check the id field in the url</h3>
-			</div>
-		);
-	}
+  if (!props.isValidSteamId) {
+    return (
+      <div className={styles.invalidPlayer}>
+        <h1>Invalid Steam ID</h1>
+        <h3>Please check the id field in the url</h3>
+      </div>
+    );
+  }
 
-	return (
-		<div className={styles.main}>
-			<NextSeo title="Overlay" />
-			<PlayerStats />
-			<SongInfo />
-			<ScoreStats />
-			<CutStats />
-		</div>
-	);
+  return (
+    <div className={styles.main}>
+      <NextSeo title="Overlay" />
+      <PlayerStats />
+      <SongInfo />
+      <ScoreStats />
+      <CutStats />
+
+      <p
+        style={{
+          fontSize: 50,
+          position: "absolute",
+          top: 0,
+          right: 0,
+          paddingRight: "10px",
+        }}
+      >
+        This overlay is deprecated use:{" "}
+        <a href="https://ssr.fascinated.cc/overlay/builder">
+          https://ssr.fascinated.cc/overlay/builder
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-	const steamId = context.query.id;
-	const steamIdResponse = await axios.get(
-		`${process.env.REACT_APP_SITE_URL}/api/validateid?steamid=${steamId}`
-	);
+  const steamId = context.query.id;
+  const steamIdResponse = await axios.get(
+    `${process.env.REACT_APP_SITE_URL}/api/validateid?steamid=${steamId}`
+  );
 
-	return {
-		props: {
-			isValidSteamId: steamIdResponse.data.message === "Valid",
-			query: JSON.stringify(context.query),
-		},
-	};
+  return {
+    props: {
+      isValidSteamId: steamIdResponse.data.message === "Valid",
+      query: JSON.stringify(context.query),
+    },
+  };
 }
